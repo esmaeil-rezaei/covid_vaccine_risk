2bcab289f3bd0f14aba54b1b7e741bdf0b756891
EsmaeilHub <esmaeilrezaei68@gmail.com>
2025-04-09 23:50:29 -0400

    data transformation and preprocessing

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-# from src.components.data_transformation import DataTransformation
-# from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_preproccessing import data_preprocessing
 
 # from src.components.model_trainer import ModelTrainerConfig
 # from src.components.model_trainer import ModelTrainer
+
+
 
 from src.utils import fetch_firebase_json_as_dataframe
 
@@ -44,10 +47,13 @@ class DataIngestion:
             )
             logging.info("Read the dataset as dataframe")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            logging.info("Train test split initiated")
+
+            df = data_preprocessing(df)
+            logging.info("Preprocessing done")
+
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            logging.info("Train test split done")
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
@@ -66,8 +72,8 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    # data_transformation=DataTransformation()
-    # train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     # modeltrainer=ModelTrainer()
     # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,17 @@
+
+import os
+import numpy as np 
 import requests
 import pandas as pd
 from src.exception_handler import handle_exception
 from src.logger import logging
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+
+
 
 def fetch_firebase_json_as_dataframe(firebase_url: str) -> pd.DataFrame:
     """
@@ -26,5 +36,79 @@ def fetch_firebase_json_as_dataframe(firebase_url: str) -> pd.DataFrame:
 
         return df
 
+    except Exception as e:
+        raise handle_exception(e)
+    
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise handle_exception(e)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise handle_exception(e)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise handle_exception(e)
+
+def binary_encoder(df, heart_attack_date, target_column_name):
+    try:
+        df[target_column_name] = df[heart_attack_date].apply(lambda x: 1 if x == 'Yes' else 0)
+        return df
+    except Exception as e:
+        raise handle_exception(e)
+    
+
+def date_feature_extractor(df, date_column):
+    try:
+        df[date_column] = pd.to_datetime(df[date_column], errors='coerce')
+        df[f'{date_column}_year'] = df[date_column].dt.year
+        df[f'{date_column}_month'] = df[date_column].dt.month
+        df[f'{date_column}_day'] = df[date_column].dt.day
+        df[f'{date_column}_day_of_week'] = df[date_column].dt.dayofweek  # Monday=0, Sunday=6
+        df[f'{date_column}_quarter'] = df[date_column].dt.quarter  # 1, 2, 3, or 4
+        df[f'{date_column}_is_weekend'] = (df[date_column].dt.weekday >= 5).astype(int)  # 1 for weekend, 0 for weekdays
+        df.drop(columns=[date_column], inplace=True)
+        return df
     except Exception as e:
         raise handle_exception(e)
